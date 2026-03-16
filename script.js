69645d0ad8f67bb79f8825fa19f1dc3dbbc0b394
Aditya-Nithesh <25bcya04@kristujayanti.com>
2026-03-16 13:35:52 +0530

    added the project to github

---
+++ script.js
@@ -0,0 +1,32 @@
+const form = document.getElementById("contactForm");
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    try {
+        const response = await fetch("https://main-project-u0vl.onrender.com/submit", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ name, email, message })
+        });
+
+        const result = await response.text();
+
+        msg.style.color = "lightgreen";
+        msg.textContent = result;
+
+        form.reset();
+
+    } catch (error) {
+        msg.style.color = "red";
+        msg.textContent = "Failed to send message!";
+        console.error(error);
+    }
+});
